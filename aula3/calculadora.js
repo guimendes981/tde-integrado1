25943c3ffb4fcd742d78aba6d063dfe7f2cf717b
Guilherme Mendes Barnabe <guilherme.barnabe@grupointegrado.br>
2023-05-08 21:23:42 -0300

    aula3

---
+++ aula3/calculadora.js
@@ -0,0 +1,23 @@
+const soma = (a,b)) => {
+    return a + b;
+};
+
+const subtracao = (a,b) => {
+    return a - b;
+}
+
+const multiplicacao = (a,b) => {
+    return a * b;
+}
+
+const divisao = (a,b) => {
+    return a / b;
+}
+
+module.exports = {
+    soma,
+    subtracao,
+    multiplicacao,
+    divisao
+}
+
---
+++ aula3/math-teste.js
@@ -0,0 +1,2 @@
+const calculadora = require('./calculadora');
+
